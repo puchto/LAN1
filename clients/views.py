7cbf2aad4d0baadb2c13c557b93a18b2cfa6b0d6
puchto <piotr.uchto@gmail.com>
2017-02-13 19:31:47 +0100

    css first half

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from clients.models import Auth_record
 
 # Create your views here.
 
 def clients_list(request):
-    return render(request, 'clients/clients_list.html', {})
+	clients_active = Auth_record.objects.filter(kl_service = 0).order_by('cl_address')
+	return render(request, 'clients/clients_list.html', {'clients': clients_active})
